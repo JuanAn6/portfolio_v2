0643bba0d96b7664e974c0421c65eefaa259210f
JuanAn6 <juanantoniorizija666@gmail.com>
2025-12-04 13:41:12 +0100

    Add i18n configuration to translate pages

---
+++ src/i18n/en.ts
@@ -0,0 +1,23 @@
+export default {
+    languageLabel: "Language",
+    languageOptions: {
+        es: "Spanish",
+        en: "English"
+    },
+    nav: {
+        home: "Home",
+        projects: "Projects",
+        about_me: "About me",
+        portfolio: "Portfolio",
+    },
+    home: {
+        title: "Welcome to Astro",
+        subtitle: "A fast framework for the web",
+        heroText: "Explore the universe with Astro",
+        infoText: "This site is fully translated.",
+    },
+    about: {
+        title: "About this project",
+        content: "This site is an example of internationalization with Astro.",
+    }
+};
---
+++ src/i18n/es.ts
@@ -0,0 +1,23 @@
+export default {
+    languageLabel: "Idioma",
+    languageOptions: {
+        es: "Español",
+        en: "Inglés"
+    },
+    nav: {
+        home: "Inicio",
+        projects: "Proyectos",
+        about_me: "Sobre mí",
+        portfolio: "Portafolio",
+    },
+    home: {
+        title: "Hola, soy Juan Antonio García Jiménez",
+        subtitle: "Desarrollador de aplicaciones web y miltiplataforma",
+        heroText: "Explora el universo con Astro",
+        infoText: "Este sitio está completamente traducido.",
+    },
+    about: {
+        title: "Acerca de este proyecto",
+        content: "Este sitio es un ejemplo de internacionalización con Astro.",
+    }
+};
