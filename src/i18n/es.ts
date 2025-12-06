22e61b3006ab8f77545ded58b531a2662881013c
JuanAn6 <juanantoniorizija666@gmail.com>
2025-12-06 16:45:53 +0100

    Add component BoxText for paragraphs in a box with shadow

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -13,8 +13,9 @@ export default {
     home: {
         title: "Hola, soy Juan Antonio García Jiménez",
         subtitle: "Desarrollador de aplicaciones web y miltiplataforma",
-        heroText: "Explora el universo con Astro",
-        infoText: "Este sitio está completamente traducido.",
+        introduce_my_self: "Cuento con más de dos años de experiencia en el desarrollo de aplicaciones web, trabajando desde la lógica del backend y la base de datos hasta la construcción de interfaces claras y funcionales.",
+        introduce_my_characteristics: "Me caracterizo por mantener una actitud proactiva, curiosa y exigente con mi propio trabajo. Siempre busco aprender más, mejorar mis habilidades y aportar valor real en cada proyecto.",
+        introduce_my_objectives: "Mi objetivo es seguir creciendo profesionalmente mientras contribuyo en equipos y productos que tengan impacto.",
     },
     about: {
         title: "Acerca de este proyecto",
