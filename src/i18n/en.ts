ea63cdd9b08e41f7ecae49bf7ddcdbd555ba86c6
JuanAn6 <juanantoniorizija666@gmail.com>
2025-12-14 14:15:10 +0100

    Add some translations and indivudual project page

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -11,10 +11,11 @@ export default {
         portfolio: "Portfolio",
     },
     home: {
-        title: "Welcome to Astro",
-        subtitle: "A fast framework for the web",
-        heroText: "Explore the universe with Astro",
-        infoText: "This site is fully translated.",
+        title: "Hello, I'm Juan Antonio García Jiménez",
+        subtitle: "Web and cross platform developer",
+        introduce_my_self: "I have over two years of experience in web application development, working from backend logic and database to building clear and functional interfaces.",
+        introduce_my_characteristics: "I am characterized by maintaining a proactive, curious, and demanding attitude toward my own work. I always seek to learn more, improve my skills, and contribute real value to every project.",
+        introduce_my_objectives: "My goal is to continue growing professionally while contributing on teams and products that have an impact.",
     },
     about: {
         title: "About this project",
@@ -22,6 +23,7 @@ export default {
     },
     projects: {
         title: "Projects",
+        show_details:"Show details",
         introduction: "This is a small collection of projects I have done during my years of study and during my free time while I am working."
     }
 };
