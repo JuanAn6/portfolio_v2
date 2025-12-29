afc71846584adc86557fbc2b03867f91bbb0012c
JuanAn6 <juanantoniorizija666@gmail.com>
2025-12-29 20:58:11 +0100

    Fix urls 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,5 @@ export default defineConfig({
     }
   },
   site: 'https://JuanAn6.github.io',
-  base: process.env.NODE_ENV === 'production' ? './portfolio_v2' : './',
+  base: process.env.NODE_ENV === 'production' ? './portfolio_v2/' : './',
 });
