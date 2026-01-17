b8e5d8d67b6b5e07256167e8765786d65083bd65
JuanAn6 <juanantoniorizija666@gmail.com>
2026-01-17 19:12:52 +0100

    Layouts for info data and technical data from a project

--- src/content/projects_es.ts
+++ src/content/projects_es.ts
@@ -3,6 +3,6 @@ export default [
     //     title: 'Example project',
     //     description: 'Diseño y desarrollo de una plataforma web auto-servicio que permite a los usuarios gestionar sus recursos en la nube de manera',
     //     img: 'https://picsum.photos/400/300?greyscale&random=4',
-    //     link: 'example_project',
+    //     link: 'example_project/info',
     // },
 ]
